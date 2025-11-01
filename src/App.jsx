d0444dff395c66f5f874daff56f384e2a22d0125
Ramaa-Devs <diwan.123140116@student.itera.ac.id>
2025-11-01 09:12:18 +0700

    Percobaan awal App.jsx dan Main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Hapus semua isi file lama, ganti dengan ini:
+
+function App() {
+  return (
+    <>
+      <h1>Aplikasi Perpustakaan Buku</h1>
+      <footer>
+        <p>Develop by Diwan Ramadhani Dwi Putra &copy;UTS Pemweb 2025 (123140116)</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
