e16b57ded38022f1d6be0c73c87a602dca206874
Ramaa-Devs <diwan.123140116@student.itera.ac.id>
2025-11-02 09:52:41 +0700

    Add logo in Header.jsx and Update App.css untuk styling logo dan font tampilan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-function Header() {
+function Header({ theme, toggleTheme }) {
   return (
     <header className="app-header">
-      <h1>LibSearch Aplikasi Pencarian Buku</h1>
-      <p>Developed by Diwan Ramadhani Dwi Putra <br/>&copy;UTS Pemweb 2025 (123140116)</p>
+      <div className="header-title-container">
+        <img 
+          src="/public/logo_trans.png" // Pastikan nama file iniSAMA dengan file di folder public
+          alt="Logo Aplikasi" 
+          className="app-logo" 
+        />
+        
+        <div className="header-text">
+          <h1>LibSearch Aplikasi Pencarian Buku</h1>
+          <p>Developed by Diwan Ramadhani Dwi Putra <br/>&copy;UTS Pemweb 2025 (123140116)</p>
+        </div>
+      </div>
+      
     </header>
   );
 }
